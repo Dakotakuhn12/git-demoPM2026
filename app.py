4e1d21ae181f7a273f7407646ffeee593e2a2321
Nadroj1234 <jordank06759@gmail.com>
2026-01-07 12:49:17 -0500

    Update print statements in app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 print("Git/Github Demo")
-print("hello from the local repo")
+print("hello from the local repo 2")
 
-print("hello from the remote repo")
+print("hello from the remote repo 3")
+print("hey local, you aint tuff")
